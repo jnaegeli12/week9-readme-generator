484215c05eeda4b61a53522e68e66d3e3905a496
Judy Naegeli <jnaegeli@gmail.com>
2020-08-15 20:30:24 -0700

    initial commit, mostly functional, still needs some formatting tweaks

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# week9-readme-generator
+# README Generator
+## Description
+So much description and really worth reading
+## Table of Contents
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
+## Installation
+Use `npm i` to install the dependencies.
+## Usage
+## License
+The Unlicense
+## Contributing
+## Tests
+Use `npm test` to test the application.
+## Questions?
+jnaegeli12<br />
+jnaegeli@gmail.com
---
+++ index.js
@@ -0,0 +1,111 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your Github profile name?",
+    name: "name"
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Please describe your project.",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "What command should be used to run tests on this application?",
+    name: "test"
+  },
+  {
+    type: "input",
+    message: "How do you use this application?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Who contributed to your project?",
+    name: "contributors"
+  },
+  {
+    type: "list",
+    message: "What license is your project under?",
+    name: "license",
+    choices: [
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "GNU LGPLv3",
+      "Mozilla Public License 2.0",
+      "Apache License 2.0",
+      "MIT License",
+      "Boost Software License 1.0",
+      "The Unlicense"
+    ]
+  }
+
+]).then(answers => {
+
+  fs.writeFile("README.md", 
+    "# " + answers.title + 
+    '\n' + "## Description" + 
+    '\n' + answers.description + 
+    '\n' + "## Table of Contents" + 
+    '\n' + "* Installation" + 
+    '\n' + "* Usage" + 
+    '\n' + "* License" + 
+    '\n' + "* Contributing" + 
+    '\n' + "* Tests" + 
+    '\n' + "* Questions" + 
+    '\n' + "## Installation" + 
+    '\n' + "Use `" + answers.installation + "` to install the dependencies." + 
+    '\n' + "## Usage" + 
+    '\n' + answers.usage + 
+    '\n' + "## License" + 
+    '\n' + answers.license + 
+    '\n' + "## Contributing" + 
+    '\n' + "## Tests" + 
+    '\n' + "Use `" + answers.test + "` to test the application." + 
+    '\n' + "## Questions?" + 
+    '\n' + answers.name + "<br />" + 
+    '\n' + answers.email,
+    
+    function (err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+
+});
+
+// fs.writeFile("log.txt", JSON.stringify(data, null, '\t'), function(err) {
+
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   console.log("Success!");
+
+// });
+
+// function to initialize program
+// function init() {
+
+// }
+
+// function call to initialize program
+// init();
